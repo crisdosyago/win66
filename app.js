9f2eca04ab9e5e3bd6a184965cb995bc09301fa9
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-05 01:38:54 +0000

    "OK"

---
+++ app.js
@@ -0,0 +1,82 @@
+import {update, merge} from './web_modules/bulgogi.js';
+
+const State = {};
+
+const render = newFlatState => (merge(State, newFlatState), update(App, newFlatState, {useBody:true}));
+
+start();
+
+function start() {
+  updateTime();
+  setInterval(updateTime, 7500);
+}
+
+function updateTime() {
+  const now = new Date;
+  const datetime = now.toISOString();
+  const date = now.toLocaleDateString();
+  const time = now.toLocaleTimeString([], {hour:'2-digit', minute:'2-digit'});
+
+  render({
+    now: { datetime, date, time }
+  });
+}
+
+function App(state) {
+  return `
+    <body>
+      <main class=desktop>
+        <article class="file folder" tabindex=0>
+          Trash
+        </article>
+        <article class=file tabindex=0>
+          File.Txt
+        </article>
+
+      </main>
+      <nav class=footer>
+        <section class=main-popups>
+          <section class=win-button-popup>
+            <button type=button name=win66 class=main>
+              &#x229e; 
+            </button>
+            <article class="main popup">
+
+            </article>
+          </section>
+          <section class=search-box-popup>
+            <input type=text name=search autocomplete=off class=box placeholder="Type here to search">
+            <span class=search-icon>&#x2315;</span>
+            <aside class=popup>
+
+            </aside>
+          </section>
+        </section>
+        <section class=tasks-and-pins>
+
+        </section>
+        <section class=status-and-infos>
+          <div class=battery>
+
+          </div>
+          <div class=internet-connection>
+
+          </div>
+          <div class=sound>
+          
+          </div>
+          <div class=language>
+
+          </div>
+          <div class=date-and-time>
+            <time datetime=${state.now.datetime}>${state.now.time}</time>
+            <time datetime=${state.now.datetime}>${state.now.date}</time>
+          </div>
+          <div class=notifications>
+            <button>&#x2709;</button>
+          </div>
+        </section>
+      </nav>
+    </body>
+  `;
+}
---
+++ web_modules/bulgogi.js
@@ -0,0 +1,101 @@
+const parser = new DOMParser();
+
+const consistentFocus = imperfectlySaveAndRestoreFocus();
+
+function toDOM(markup) {
+  return parser.parseFromString(markup, 'text/html');
+}
+
+function update(view, state, {useBody: useBody = false} = {}) {
+  consistentFocus.next();
+  
+  const doc = toDOM(view(state));
+
+  if ( useBody ) {
+    document.body.replaceWith(doc.body);
+  } else {
+    document.documentElement.replaceWith(doc.documentElement);
+  }
+
+  consistentFocus.next();
+}
+
+// state merging export
+  function merge(state, newStateFlat) {
+    // Notes
+      // normally if we use Object.assign(state, newState), then
+      // nested properties not specified in newState will be overwritten
+      // so merge does a proper deep merge 
+      // but it does it an easy way by requiring all nested property chains 
+      // are already converted to flat strings
+      // like prop1.prop2.prop3
+    [...Object.entries(newStateFlat)].forEach(([key, value]) => {
+      const path = key.split('.');
+      const lastStep = path.pop();
+      let root = state;
+      for( const step of path ) {
+        if ( ! root[step] ) {
+          // fill in gaps
+          root = {};
+        } else {
+          root = root[step];
+        }
+      }
+      root[lastStep] = value;
+    });
+  }
+
+// utility exports
+  function clone(o) {
+    return JSON.parse(JSON.stringify(o));
+  }
+
+// rendering helpers
+// imperfect focus restoring helpers
+	// we need helpers to keep focus where it was before the last render
+	function *imperfectlySaveAndRestoreFocus() {
+		// we use a generator to track focus because 
+		// generators make it easy to keep track of 
+		// state over a repetitve asynchronous process
+
+		while(true) {
+			const active = document.activeElement;
+			let selectionStart,selectionEnd,value;
+
+			if ( active && (active instanceof HTMLInputElement || active instanceof HTMLTextAreaElement) ) {
+				({selectionStart,selectionEnd,value} = active);
+			}
+
+			yield;
+
+			// we don't want to preserve focus across route changes
+			if ( active ) {
+				const newActive = document.querySelector(imperfectlyGetSelector(active));
+				if ( newActive ) {
+					newActive.focus();
+					try {
+						Object.assign(newActive,{selectionStart,selectionEnd});
+					} catch(e) {}
+				}
+			}
+
+			yield;
+		}
+	}
+
+	function imperfectlyGetSelector(el) {
+		// the first html to our selector does not help specificity
+		return `${
+			el.parentElement && el.parentElement.localName != 'html' ? `${imperfectlyGetSelector(el.parentElement)} > ` : ''  
+		}${
+			el.localName
+		}${
+			el.id ? `#${el.id.replace(/\./g, '\\\\.')}` : ''
+		}${
+			el.classList.length ? `.${[...el.classList].join('.')}` : ''
+		}${
+			el.name ? `[name="${el.name}"]` : ''
+		}`;
+	}
+
+export { clone, merge, update };
