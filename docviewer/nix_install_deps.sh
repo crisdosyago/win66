73579c83c2214543b18e9abf23985bda8114c5f7
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-24 11:01:45 +0000

    "Added p2."

---
+++ docviewer/nix_install_deps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# assumes freebsd
+
+sudo apt -y install ghostscript9-agpl-base-9.27_2
+# sudo apt -y install ghostscript
+sudo apt -y install print/tex-dvipsk
+# sudo apt -y install texlive-base
+sudo apt -y install ImageMagick7-nox11-7.0.8.57_1 
+# sudo apt -y install imagemagick
+sudo apt -y install libreoffice
+# sudo apt -y install ffmpeg
+
---
+++ docviewer/scripts/convert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo $1
+
+base=$2
+format=$3
+
+if [ -z $format ]; then
+  format="png"
+fi
+
+cp $base/index.html $1.html
+
+convert -verbose -density 100 -background ivory -alpha remove -alpha off $1 +adjoin $1-%04d.$format || (mutool draw -i -o $1-%04d.$format $1 && $base/../../scripts/rename_1_based.sh $1 $format)
+
+mv $1 $base/../../pdfs/
+
